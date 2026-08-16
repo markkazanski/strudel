6cdd1a3b3454f03276f809edf561c0fbf2d57c26
Mark Kazanski <markkazanski@gmail.com>
2026-08-16 18:05:34 -0500

    Kznski

---
+++ Kznski/Kznski_midi.strudel.js
@@ -0,0 +1,93 @@
+setcpm(91/4)
+
+$: note(`<
+    [a3 a3 a3 a3 a3 a3 a3 b3] [b3 b3 b3 b3 b3 g3 f3] [f3 f3 f3 f3 f3 f3 d4]
+    [d4 d4 d4 d4 d4 g3 a3] [a3 a3 a3 a3 a3 a3 a3 b3] [b3 b3 b3 b3 b3 g3 f3]
+    [f3 f3 f3 f3 f3 f3 d4] [d4 d4 d4 d4 d4 g3 a3] [a3 a3 a3 a3 a3 a3 a3 b3]
+    [b3 b3 b3 b3 b3 g3 f3] [f3 f3 f3 f3 f3 f3 d4] [d4 d4 d4 d4 d4 g3 a3]
+    [a3 a3 a3 a3 a3 a3 a3 b3] [b3 b3 b3 b3 b3 g3 f3] [f3 f3 f3 f3 f3 f3 d4]
+    [f3 f3 f3 f3 f3 f3 f3 g3] [g3 g3 g3 g3 g3 e3 e3] [e3 e3 e3 e3 e3 e3 f3]
+    [f3 f3 f3 f3 f3 f3 f3 f3] [f3 f3 f3 f3 f3 f3 f3 g3] [g3 g3 g3 g3 g3 e3 e3]
+    [e3 e3 e3 e3 e3 e3 f3] [f3 f3 f3 f3 f3 f3 f3] [f3 a3 f3 a3 f3 a3 f3 a3 f3 a3 f3 a3 f3 a3 g3 b3]
+    [g3 b3 g3 b3 g3 b3 g3 b3 g3 b3 e3 g3 e3 f3] [e3 f3 e3 f3 e3 f3 e3 f3 e3 f3 e3 f3 f3 d4] [f3 d4 f3 d4 f3 d4 f3 d4 f3 d4 f3 g3 f3 a3]
+    - - -
+    [f3 f3 f3 f3 f3 f3 f3 g3] [g3 g3 g3 g3 g3 e3 e3] [f3 f3 f3 f3 f3 f3 f3 g3]
+    [g3 g3 g3 g3 g3 e3 e3]>`)
+  .sound("piano") // bass?
+
+$: note(`<
+    [a4 a4 b4] [g4 f4] [f4 e4]
+    [g4 a4] [a4 a4 b4] [g4 f4]
+    [f4 e4] [g4 a4] [a4 a4 b4]
+    [g4 f4] [f4 e4] [g4 a4]
+    [a4 a4 b4] [g4 f4] [f4 e4]
+    [a4 a4 b4] [g4 g4] g4
+    [a4 a4] [a4 a4 b4] [g4 g4]
+    g4 a4 [a4 a4 a4 a4 b4 b4]
+    [g4 g4 g4 f4] [g4 f4 e4] [a4 g4 a4]>`)
+  .sound("saw") // Bass 2
+
+$: note(`<
+    [g4 a4 c5 g4 a4 c5 g4 a4] [f4 b4 d5 f4 b4 d5 f4 b4] [c4 f4 a4 c4 f4 a4 c4 f4]
+    [d4 f4 a4 d4 f4 a4 d4 f4] [g4 a4 c5 g4 a4 c5 g4 a4] [f4 b4 d5 f4 b4 d5 f4 b4]
+    [c4 f4 a4 c4 f4 a4 c4 f4 d4] [d4 f4 a4 d4 f4 a4 d4 f4] [g4 a4 c5 g4 a4 c5 g4 a4]
+    [f4 b4 d5 f4 b4 d5 f4 b4 c4] [c4 f4 a4 c4 f4 a4 c4 f4] [d4 f4 a4 d4 f4 a4 d4 f4]
+    [g4 a4 c5 g4 a4 c5 g4 a4] [f4 b4 d5 f4 b4 d5 f4 b4 c4] [c4 f4 a4 c4 f4 a4 c4 f4]
+    [a4 c5 e5 a4 c5 e5 c5] [b4 d5 f5 b4 d5 f5 d5] [e4 g4 b4 e4 g4 b4 g4]
+    [f4 a4 c5 f4 a4 c5 a4 a4] [a4 c5 e5 a4 c5 e5 c5] [b4 d5 f5 b4 d5 f5 d5]
+    [e4 g4 b4 e4 g4 b4 g4] [f4 a4 c5 f4 a4 c5 a4] [a4 g4 c5 a4 e5 c5 a4 g4 c5 a4 e5 c5 g4 c5 a4]
+    [b4 f4 d5 b4 f5 d5 b4 f4 d5 b4 f5 d5 f4 d5 b4] [e4 c4 g4 f4 b4 a4 e4 c4 g4 f4 b4 a4 c4 g4 f4] [f4 d4 a4 f4 c5 a4 f4 d4 a4 f4 c5 a4 d4 a4 f4]
+    - - -
+    [a4 c5 e5 a4 c5 e5 c5] [b4 d5 f5 b4 d5 f5 d5] [a4 c5 e5 a4 c5 e5 c5]
+    [b4 d5 f5 b4 d5 f5 d5]>`)
+  .sound("gm_lead_3_calliope")
+
+$: note(`<
+    [g4 a4 c5 g4 a4 c5 g4 a4] [f4 b4 d5 f4 b4 d5 f4 b4] [c4 f4 a4 c4 f4 a4 c4 f4]
+    [d4 f4 a4 d4 f4 a4 d4 f4] [g4 a4 c5 g4 a4 c5 g4 a4] [f4 b4 d5 f4 b4 d5 f4 b4]
+    [c4 f4 a4 c4 f4 a4 c4 f4 d4] [d4 f4 a4 d4 f4 a4 d4 f4] [g4 a4 c5 g4 a4 c5 g4 a4]
+    [f4 b4 d5 f4 b4 d5 f4 b4 c4] [c4 f4 a4 c4 f4 a4 c4 f4] [d4 f4 a4 d4 f4 a4 d4 f4]
+    [g4 a4 c5 g4 a4 c5 g4 a4] [f4 b4 d5 f4 b4 d5 f4 b4 c4] [c4 f4 a4 c4 f4 a4 c4 f4]
+    [a4 c5 e4 b4 d5 f4] [g4 b4 d4] [a4 c5 e4]
+    [a4 c5 e4] [a4 c5 e4 b4 d5 f4] [g4 b4 d4]
+    [a4 c5 e4] - [a4 c5 e4 g4 a4 c5 g4 a4 c5 g4 b4 d5 f4 a4]
+    [f4 b4 d5 f4 b4 d5 f4 g4 b4 d4 b4] [c4 f4 a4 c4 f4 a4 c4 a4 c5 e4 f4] [d4 f4 a4 d4 f4 a4 d4 f4]>`)
+  .sound("gm_lead_4_chiff")
+
+$: note(`<
+    - - -
+    [a5 a5 a5] [c6 c6 c6 c6 c6 b5] [b5 b5 b5 a5]
+    [a5 g5 f5] [f5 f5 a5 a5 a5] [c6 c6 c6 c6 b5]
+    [b5 b5 a5] [a5 g5 f5] [f5 f5 g5 a5 a5 e5 a5]
+    [c6 c6 c6 c6 c6 b5] [b5 b5 b5 a5] [a5 g5 f5]
+    [a4 e5 d5 c5 d5] e5 [g4 e5 d5 c5 c5]
+    - [a4 e5 d5 c5 d5] [b4 a4]
+    [g4 g4 g4 g4 c5] a4 [c6 c6 c6 a4 c6 e5 d5 c5 c6 d5 b5]
+    [b4 b5 a4 b5 b5 a5] [g4 g4 a5 g4 g5 g4 c5 f5] [f5 f5 a4 a5 a4 a5 a4 a5 c6]
+    [c6 c6 c6 c6 b5] [b5 b5 a5] [a5 g5 f5]>`)
+  .sound("gm_lead_5_charang")
+
+$: note(`<
+    [c2 c#2 g2 c2 g2 c#2] [c2 c#2 g2 c2 g2 c#2] [c2 c#2 g2 c2 g2 c#2]
+    [c2 c#2 g2 c2 g2 c#2] [c2 c#2 g2 c2 g2 c#2] [c2 c#2 g2 c2 g2 c#2]
+    [c2 c#2 g2 c2 g2 c#2 c2] [c2 c#2 g2 c2 g2 c#2] [c2 c#2 g2 c2 g2 c#2]
+    [c2 c#2 g2 c2 g2 c#2 c2] [c2 c#2 g2 c2 g2 c#2] [c2 c#2 g2 c2 g2 c#2]
+    [c2 c#2 g2 c2 g2 c#2] [c2 c#2 g2 c2 g2 c#2 c2] [c2 c#2 g2 c2 g2 c#2]
+    [c2 c#2 g2 c2 g2 c#2] [c2 c#2 g2 c2 g2 c#2] [c2 c#2 g2 c2 g2 c#2]
+    [c2 c#2 g2 c2 g2 c#2 c2] [c2 c#2 g2 c2 g2 c#2] [c2 c#2 g2 c2 g2 c#2]
+    [c2 c#2 g2 c2 g2 c#2] [c2 c#2 g2 c2 g2 c#2] [c2 c2 c#2 g2 c#2 g2 c2 c2 g2 c#2 g2 c#2]
+    [c2 c2 c#2 g2 c#2 g2 c2 c2 g2 c#2 g2 c#2] [c2 c2 c#2 g2 c#2 g2 c2 c2 g2 c#2 g2 c#2] [c2 c2 c#2 g2 c#2 g2 c2 c2 g2 c#2 g2 c#2]
+    - - -
+    [c2 c#2 g2 c2 g2 c#2] [c2 c#2 g2 c2 g2 c#2]>`)
+  .sound("gm_lead_6_voice")
+
+$: note(`<
+    - - -
+    - - -
+    - - -
+    - - -
+    - - -
+    - - -
+    g2 g2 -
+    - - g2>`)
+  .sound("gm_lead_7_fifths")
